4cf8d8fcf07aa770d2a20b65b212d894df653c84
cxde-sujal <sujalsinha10@gmail.com>
2024-09-04 23:04:20 +0530

    Div3 A

---
+++ legs.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class legs{
+    public static void main(String args[]){
+        Scanner in=new Scanner(System.in);
+        int t=in.nextInt();
+        while(t-- >0){
+            int n=in.nextInt();
+            int c=0;
+            while(n>=4){
+                c++;
+                n-=4;
+            }
+            if(n!=0){
+                c++;
+            }
+            System.out.println(c);
+        }
+        in.close();
+    }
+}
